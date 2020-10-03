8369ab50eb3f9ef9099fa1f2cd14f63e70813bdc
andriyan.krastev <andriyan.krastev@gmail.com>
2020-10-03 03:11:25 +0300

    added IDisposable project.

---
+++ src/IDisposable/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using IDisposable.Before;
+
+namespace IDisposable
+{
+    class Program
+    {
+        private static DatabaseState _DatabaseState;
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("'g' to Get Date; 'gc' to Garbage Collect; 'x' to Exit");
+            var command = "";
+            while (command != "x")
+            {
+                command = Console.ReadLine();
+                switch (command)
+                {
+                    case "g":
+                        GetDate();
+                        break;
+
+                    case "gc":
+                        GC.Collect();
+                        break;
+                }
+            }
+        }
+        
+        private static void GetDate()
+        {
+            // Before
+            // if (_DatabaseState == null)
+            // {
+            //     _DatabaseState = new DatabaseState();
+            // }
+            // Console.WriteLine(_DatabaseState.GetDate());
+            
+            // After
+            // using (var databaseState = new DatabaseState())
+            //     //var databaseState = new DatabaseState();
+            // {
+            //     Console.WriteLine(databaseState.GetDate());
+            // }
+        }
+    }
+}
