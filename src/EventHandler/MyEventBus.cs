a8ad77a9e2958b406aef22f2439041a2011f1772
chunk1ty <andriyan.krastev@gmail.com>
2020-10-17 03:16:47 +0300

    play with EventHandler and Action

---
+++ src/EventHandler/MyEventBus.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EventHandler
+{
+    public class MessageArgs : EventArgs
+    {
+        public MessageArgs(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; }
+
+        public override string ToString()
+        {
+            return $"Message {{Id: {Id}}}";
+        }
+    }
+
+    public class MyEventBus
+    {
+        public event EventHandler<MessageArgs> MessageHandler;
+
+        public void SubscribeEventHandler()
+        {
+            Task.Run(async () => await PublishMessages());   
+        }
+        
+        private async Task PublishMessages()
+        {
+            Random random = new Random();
+
+            while (true)
+            {
+                var message = new MessageArgs(random.Next());
+
+                Console.WriteLine(message);
+                MessageHandler?.Invoke(this, message);
+
+                await Task.Delay(2000);
+            }
+        }
+        
+        public void SubscribeAction(Action<MessageArgs> action)
+        {
+            Task.Run(async () => await PublishMessages1(action));   
+        }
+        
+        private async Task PublishMessages1(Action<MessageArgs> action)
+        {
+            Random random = new Random();
+
+            while (true)
+            {
+                var message = new MessageArgs(random.Next());
+
+                Console.WriteLine(message);
+                action(message);
+
+                await Task.Delay(2000);
+            }
+        }
+    }
+}
